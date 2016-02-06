332f9979515f15cd16bda6c94b9e1d616dc65f5a
Eric Hebert <ayebear@gmail.com>
2016-02-05 22:20:05 -0500

    Added components file (just comments) Added Kran ECS library

---
+++ client/data/src/components.js
@@ -0,0 +1,24 @@
+/*
+Input handling:
+	MouseControlled
+		(Sets Position)
+	KeyDown
+
+Core logic:
+	Position
+	Velocity
+	Destination
+		(Later extended to Path)
+	Sprite
+	Health
+	Shield
+		(Similar to health)
+	Selectable
+		(Unit mode and structure mode)
+
+Unit-specific:
+	Targeter
+	Damager
+	Booster
+
+*/
